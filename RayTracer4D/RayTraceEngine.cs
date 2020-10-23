2dae346314ee586487389ca510688b39b46298a5
bhespiritu <lozman1@elne.org>
2020-10-23 18:48:24 -0400

    working Veldrid example

--- RayTracer4D/RayTraceEngine.cs
+++ RayTracer4D/RayTraceEngine.cs
@@ -2,6 +2,10 @@
 using Veldrid;
 using Veldrid.Sdl2;
 using Veldrid.StartupUtilities;
+using Veldrid.SPIRV;
+using System.Numerics;
+using System.Text;
+using System.Diagnostics;
 
 namespace RayTracer4D
 {
@@ -11,8 +15,12 @@ namespace RayTracer4D
         private static CommandList _commandList;
         private static DeviceBuffer _vertexBuffer;
         private static DeviceBuffer _indexBuffer;
+        private static DeviceBuffer _uniformBuffer;
         private static Shader[] _shaders;
         private static Pipeline _pipeline;
+
+        private static Stopwatch stopwatch;
+        private static double[] time = { 0,0 };
 
         static void Main(string[] args)
         {
@@ -26,10 +34,163 @@ namespace RayTracer4D
             };
             Sdl2Window window = VeldridStartup.CreateWindow(ref windowCI);
             _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window);
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            CreateResources();
+
             while (window.Exists)
             {
+                time[0] = (float)stopwatch.Elapsed.TotalSeconds;
+                _graphicsDevice.UpdateBuffer(_uniformBuffer, 0, time);
                 window.PumpEvents();
+                Draw();
+            }
+
+            DisposeResources();
+        }
+
+        
+
+        public static void Draw()
+        {
+            _commandList.Begin();
+            _commandList.SetFramebuffer(_graphicsDevice.SwapchainFramebuffer);
+            _commandList.ClearColorTarget(0, RgbaFloat.Black);
+            _commandList.SetVertexBuffer(0, _vertexBuffer);
+            _commandList.SetIndexBuffer(_indexBuffer, IndexFormat.UInt16);
+            _commandList.SetPipeline(_pipeline);
+            _commandList.DrawIndexed(
+                indexCount: 4,
+                instanceCount: 1,
+                indexStart: 0,
+                vertexOffset: 0,
+                instanceStart: 0);
+
+            _commandList.End();
+            _graphicsDevice.SubmitCommands(_commandList);
+            _graphicsDevice.SwapBuffers();
+        }
+
+        private const string VertexCode = @"
+#version 450
+
+layout(set = 0, binding = 0) uniform time
+layout(location = 0) in vec2 Position;
+layout(location = 1) in vec4 Color;
+
+layout(location = 0) out vec4 fsin_Color;
+
+void main()
+{
+    Position.y += sin(time);
+    gl_Position = vec4(Position, 0, 1);
+    fsin_Color = Color;
+}";
+
+        private const string FragmentCode = @"
+#version 450
+
+layout(location = 0) in vec4 fsin_Color;
+layout(location = 0) out vec4 fsout_Color;
+
+void main()
+{
+    fsout_Color = fsin_Color;
+}";
+
+
+        private static void CreateResources()
+        {
+            ResourceFactory factory = _graphicsDevice.ResourceFactory;
+
+            VertexPositionColor[] quadVertices =
+            {
+                new VertexPositionColor(new Vector2(-.75f, .75f), RgbaFloat.Red),
+                new VertexPositionColor(new Vector2(.75f, .75f), RgbaFloat.Green),
+                new VertexPositionColor(new Vector2(-.75f, -.75f), RgbaFloat.Blue),
+                new VertexPositionColor(new Vector2(.75f, -.75f), RgbaFloat.Yellow)
+            };
+
+            ushort[] quadIndices = { 0, 1, 2, 3 };
+
+            _vertexBuffer = factory.CreateBuffer(new BufferDescription(4 * VertexPositionColor.SizeInBytes, BufferUsage.VertexBuffer));
+            _indexBuffer = factory.CreateBuffer(new BufferDescription(4 * sizeof(ushort), BufferUsage.IndexBuffer));
+            _uniformBuffer = factory.CreateBuffer(new BufferDescription(2*sizeof(double), BufferUsage.UniformBuffer));
+
+            _graphicsDevice.UpdateBuffer(_vertexBuffer, 0, quadVertices);
+            _graphicsDevice.UpdateBuffer(_indexBuffer, 0, quadIndices);
+            _graphicsDevice.UpdateBuffer(_uniformBuffer, 0, time);
+
+
+            VertexLayoutDescription vertexLayout = new VertexLayoutDescription(
+                new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2),
+                new VertexElementDescription("Color", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float4));
+
+            ShaderDescription vertexShaderDesc = new ShaderDescription(
+                ShaderStages.Vertex,
+                Encoding.UTF8.GetBytes(VertexCode),
+                "main");
+            ShaderDescription fragmentShaderDesc = new ShaderDescription(
+                ShaderStages.Fragment,
+                Encoding.UTF8.GetBytes(FragmentCode),
+                "main");
+
+            _shaders = factory.CreateFromSpirv(vertexShaderDesc, fragmentShaderDesc);
+
+            GraphicsPipelineDescription pipelineDescription = new GraphicsPipelineDescription();
+            pipelineDescription.BlendState = BlendStateDescription.SingleOverrideBlend;
+
+            pipelineDescription.DepthStencilState = new DepthStencilStateDescription(
+                depthTestEnabled: true,
+                depthWriteEnabled: true,
+                comparisonKind: ComparisonKind.LessEqual);
+
+            pipelineDescription.RasterizerState = new RasterizerStateDescription(
+                cullMode: FaceCullMode.Back,
+                fillMode: PolygonFillMode.Solid,
+                frontFace: FrontFace.Clockwise,
+                depthClipEnabled: true,
+                scissorTestEnabled: false);
+
+            pipelineDescription.PrimitiveTopology = PrimitiveTopology.TriangleStrip;
+            pipelineDescription.ResourceLayouts = System.Array.Empty<ResourceLayout>();
+
+            pipelineDescription.ShaderSet = new ShaderSetDescription(
+                vertexLayouts: new VertexLayoutDescription[] { vertexLayout },
+                shaders: _shaders);
+
+            pipelineDescription.Outputs = _graphicsDevice.SwapchainFramebuffer.OutputDescription;
+            _pipeline = factory.CreateGraphicsPipeline(pipelineDescription);
+
+            _commandList = factory.CreateCommandList();
+        }
+
+        private static void DisposeResources()
+        {
+            _pipeline.Dispose();
+            _commandList.Dispose();
+            _vertexBuffer.Dispose();
+            _indexBuffer.Dispose();
+            _graphicsDevice.Dispose();
+            foreach(Shader s in _shaders)
+            {
+                s.Dispose();
             }
         }
+    }
+
+    
+
+    struct VertexPositionColor
+    {
+        public Vector2 Position; // This is the position, in normalized device coordinates.
+        public RgbaFloat Color; // This is the color of the vertex.
+        public VertexPositionColor(Vector2 position, RgbaFloat color)
+        {
+            Position = position;
+            Color = color;
+        }
+        public const uint SizeInBytes = 24;
     }
 }
